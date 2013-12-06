8db1466fdd22954bde291e20997a9aa3bb1fd63e
Martin Kadlec <harou2@gmail.com>
2013-12-06 11:23:29 +0100

    add documentation

---
+++ docs/api.js
@@ -0,0 +1,34 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Alba",
+        "Album",
+        "Clen",
+        "ClenJoinNastroj",
+        "Clenove",
+        "ClenoveJoinSkupiny",
+        "DefaultParams",
+        "LapiApplication",
+        "LapiCollection",
+        "LapiDatabase",
+        "LapiModel",
+        "LapiURL",
+        "Nastroj",
+        "Nastroje",
+        "Skladba",
+        "Skladby",
+        "Skupina",
+        "SkupinaJoinClen",
+        "Skupiny",
+        "SkupinyJoinClenove",
+        "Uzivatel",
+        "Uzivatele",
+        "Vydavatel",
+        "Vydavatele",
+        "Zadost",
+        "Zadosti"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
